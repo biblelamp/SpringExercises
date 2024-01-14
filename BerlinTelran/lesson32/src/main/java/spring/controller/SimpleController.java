5291d97ef2992688d91b7e664d5dbbc1ee95593a
Sergey Iryupin <biblelamp@yandex.ru>
2024-01-14 09:06:07 +0100

    telran lesson 32

---
+++ BerlinTelran/lesson32/src/main/java/spring/controller/SimpleController.java
@@ -0,0 +1,14 @@
+package spring.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        return "index";
+    }
+}
