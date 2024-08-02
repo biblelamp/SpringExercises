f191ffac146c086156f688973c302dc0a96f14ff
Sergey Iryupin <biblelamp@yandex.ru>
2024-08-02 15:47:57 +0200

    hello kafka init

---
+++ HelloKafka/src/main/java/kafka/Application.java
@@ -0,0 +1,16 @@
+package kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * A Comprehensive Guide to Integrating Kafka in a Spring Boot Application
+ * by https://medium.com/@abhishekranjandev/a-comprehensive-guide-to-integrating-kafka-in-a-spring-boot-application-a4b912aee62e
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
