0e747a3134f9846de0143be94dd5fef175779316
Sergey Iryupin <biblelamp@yandex.ru>
2024-09-21 11:03:38 +0200

    user-profile fix errors

--- iPrody/user-profile/user-profile-app/src/main/java/com/iprody/user/profile/domain/UserProfile.java
+++ iPrody/user-profile/user-profile-app/src/main/java/com/iprody/user/profile/domain/UserProfile.java
@@ -3,13 +3,14 @@ package com.iprody.user.profile.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class UserProfile {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_id")
     private int id;
 
