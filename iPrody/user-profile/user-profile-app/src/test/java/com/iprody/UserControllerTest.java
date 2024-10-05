04d0a32cf8d9700db504734ce7a713265b56f116
Sergey Iryupin <biblelamp@yandex.ru>
2024-10-05 10:00:07 +0200

    user-profile 1st simple test

--- iPrody/user-profile/user-profile-app/src/test/java/com/iprody/UserControllerTest.java
+++ iPrody/user-profile/user-profile-app/src/test/java/com/iprody/UserControllerTest.java
@@ -1,7 +1,12 @@
 package com.iprody;
 
 import com.iprody.user.profile.Application;
+import com.iprody.user.profile.test.ApiClient;
+import com.iprody.user.profile.test.controller.UserApi;
+import com.iprody.user.profile.test.controller.dto.UserDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -9,8 +14,35 @@ import org.springframework.test.context.ContextConfiguration;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
 
+    static final String DEFAULT_URI = "http://localhost";
+    static final String REST_CONTROLLER_URL = "/api";
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private ApiClient apiClient;
+
+    private UserApi userApi;
+
+//    @BeforeAll
+//    public static void setUp() {
+//        apiClient = new ApiClient();
+//        apiClient.setBasePath("http://localhost:8080/api");
+//
+//        userApi = new UserApi(apiClient);
+//    }
+
     @Test
     public void addUser() {
-        // TODO
+        apiClient = new ApiClient();
+        apiClient.setBasePath(DEFAULT_URI + ":" + port + REST_CONTROLLER_URL);
+        userApi = new UserApi(apiClient);
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFirstName("John");
+        userDTO.setLastName("Smith");
+        Integer userId = userApi.addUser(userDTO);
+
+        Assertions.assertNotNull(userId);
     }
 }
