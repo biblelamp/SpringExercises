02f759bf240bc67a8332c2e247c93034f9a6b648
Sergey Iryupin <biblelamp@yandex.ru>
2024-10-06 15:58:14 +0200

    using @PostConstruct

--- iPrody/user-profile/user-profile-app/src/test/java/com/iprody/UserControllerTest.java
+++ iPrody/user-profile/user-profile-app/src/test/java/com/iprody/UserControllerTest.java
@@ -4,10 +4,11 @@ import com.iprody.user.profile.Application;
 import com.iprody.user.profile.test.ApiClient;
 import com.iprody.user.profile.test.controller.UserApi;
 import com.iprody.user.profile.test.controller.dto.UserDTO;
+import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(classes = Application.class)
@@ -17,27 +18,20 @@ public class UserControllerTest {
     static final String DEFAULT_URI = "http://localhost";
     static final String REST_CONTROLLER_URL = "/api";
 
-    @Value("${local.server.port}")
+    @LocalServerPort //@Value("${local.server.port}")
     private int port;
-
-    private ApiClient apiClient;
 
     private UserApi userApi;
 
-//    @BeforeAll
-//    public static void setUp() {
-//        apiClient = new ApiClient();
-//        apiClient.setBasePath("http://localhost:8080/api");
-//
-//        userApi = new UserApi(apiClient);
-//    }
+    @PostConstruct
+    public void setUp() {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(DEFAULT_URI + ":" + port + REST_CONTROLLER_URL);
+        userApi = new UserApi(apiClient);
+    }
 
     @Test
     public void addUser() {
-        apiClient = new ApiClient();
-        apiClient.setBasePath(DEFAULT_URI + ":" + port + REST_CONTROLLER_URL);
-        userApi = new UserApi(apiClient);
-
         UserDTO userDTO = new UserDTO();
         userDTO.setFirstName("John");
         userDTO.setLastName("Smith");
